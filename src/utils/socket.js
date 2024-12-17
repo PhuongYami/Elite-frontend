f52104fbf222adfd0b8652a0c96da7c3a4ba0f8f
NguyenDuyPhuongDeveloper <phuongnd0239@gmail.com>
2024-12-17 10:09:14 +0700

    Adjust

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,7 @@
 //src/utils/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io(process.env.REACT_APP_BACKEND_URL || "http://localhost:5000", {
     withCredentials: true,
 });
 
