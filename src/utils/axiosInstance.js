a2d76c760f02ec141d0bb9b32cd0d7700ae9fde7
NguyenDuyPhuongDeveloper <phuongnd0239@gmail.com>
2024-12-18 21:25:25 +0700

    Test axios

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -8,7 +8,7 @@ import { toast } from 'react-toastify';
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api/v1',
     withCredentials: true,
-    timeout: 10000,
+    timeout: 20000,
     headers: {
         'Content-Type': 'application/json',
     },
